747fb7b544b9b8e61e0a7ef220dc2750f69ab75a
chenyinjie <chenyinjie666@foxmail.com>
2025-03-04 22:49:25 +0800

    2025/03/04 add cpp_demo_pkg, py_demo_pkg

---
+++ ch2/src/cpp_demo_pkg/src/cpp_node.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("cpp_node");
+    RCLCPP_INFO(node->get_logger(), "Hello, cpp node!");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+
+    return 0;
+}
