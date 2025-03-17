44d87c1d4cde181b91fb7ff0c2651ce416519f38
chenyinjie <chenyinjie666@foxmail.com>
2025-03-17 22:22:21 +0800

    2025/03/17 init ch5 repo.

---
+++ ch5/src/py_demo_tf/py_demo_tf/static_tf_broadcaster.py
@@ -0,0 +1,8 @@
+import math
+import rclpy
+from rclpy.node import Node
+from tf2_ros import StaticTransformBroadcaster
+from geometry_msgs.msg import TransformStamped
+from tf_transformations import quaternion_from_euler
+
+
