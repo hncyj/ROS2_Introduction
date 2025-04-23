0c0eaf0a3add4820b4b0c3a563dfaf985fc01701
chenyinjie <chenyinjie666@foxmail.com>
2025-04-23 18:30:34 +0800

    2025/04/23 fix format problem.

--- ch2/src/cpp_demo_pkg/src/cpp_node.cpp
+++ ch2/src/cpp_demo_pkg/src/cpp_node.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <rclcpp/rclcpp.hpp>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<rclcpp::Node>("cpp_node");
     RCLCPP_INFO(node->get_logger(), "Hello, cpp node!");
