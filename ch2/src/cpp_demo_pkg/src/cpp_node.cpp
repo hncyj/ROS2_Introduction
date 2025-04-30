46132fdb6b14ae4ef25c21ce27f812b132a682c2
chenyinjie <chenyinjie666@foxmail.com>
2025-04-30 09:44:07 +0800

    2025/04/30 <style> revised code style.

--- ch2/src/cpp_demo_pkg/src/cpp_node.cpp
+++ ch2/src/cpp_demo_pkg/src/cpp_node.cpp
@@ -3,9 +3,11 @@
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
+    
     auto node = std::make_shared<rclcpp::Node>("cpp_node");
     RCLCPP_INFO(node->get_logger(), "Hello, cpp node!");
     rclcpp::spin(node);
+
     rclcpp::shutdown();
 
     return 0;
